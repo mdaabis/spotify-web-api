261be4badbd237ee4544e1b5d7fdd41f20c25629
mdaabis <mostafadaabis@gmail.com>
2022-10-24 00:01:16 +0100

    Add web based authentication

--- src/SpotifyMain.java
+++ src/SpotifyMain.java
@@ -2,51 +2,28 @@ import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 
 import java.net.URI;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
 
 public class SpotifyMain {
 
+	  public static String spotifyLogin() {
+		  final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+				  .setClientId(AuthorisationUtils.CLIENT_ID)
+				  .setClientSecret(AuthorisationUtils.CLIENT_SECRET)
+				  .setRedirectUri(AuthorisationUtils.redirectUri)
+				  .build();
 
-	  private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
-			    .setClientId(AuthorisationUtils.CLIENT_ID)
-			    .setClientSecret(AuthorisationUtils.CLIENT_SECRET)
-			    .setRedirectUri(AuthorisationUtils.redirectUri)
-			    .build();
-	  
-	  private static final AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
-//			          .state("x4xkmn9pu3j6ukrs8n")
-//			          .scope("user-read-birthdate,user-read-email")
-//			          .show_dialog(true)
-	    .build();
-
-	  public static void authorizationCodeUri_Sync() {
-	    final URI uri = authorizationCodeUriRequest.execute();
-
-	    System.out.println("URI: " + uri.toString());
-	  }
-
-	  public static void authorizationCodeUri_Async() {
-	    try {
-	      final CompletableFuture<URI> uriFuture = authorizationCodeUriRequest.executeAsync();
-
-	      // Thread free to do other tasks...
-
-	      // Example Only. Never block in production code.
-	      final URI uri = uriFuture.join();
-
-	      System.out.println("URI: " + uri.toString());
-	    } catch (CompletionException e) {
-	      System.out.println("Error: " + e.getCause().getMessage());
-	    } catch (CancellationException e) {
-	      System.out.println("Async operation cancelled.");
-	    }
+		  AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
+//				  .state("x4xkmn9pu3j6ukrs8n")
+//				  .scope("user-read-birthdate,user-read-email")
+//			      .show_dialog(true)
+				  .build();
+		  final URI uri = authorizationCodeUriRequest.execute();
+		  return uri.toString();
 	  }
 
 	  public static void main(String[] args) {
-	    authorizationCodeUri_Sync();
-	    authorizationCodeUri_Async();
+		  System.out.println("test: " + spotifyLogin());
+
 	  }
 
 }
